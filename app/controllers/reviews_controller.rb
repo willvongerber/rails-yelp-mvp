86ce6cc2a3131dde7dfb15179c47ef448b4cde89
Wilhelm von Gerber <willvongerber@gmail.com>
2022-11-23 16:22:35 +0100

    rails-yelp-mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,14 +8,15 @@ class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = @restaurant
 
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
+    if @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
